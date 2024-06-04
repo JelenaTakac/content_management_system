529c6dc9a025e7780164a959f8b6b175e091cc67
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-06-04 08:52:46 +0200

    Refactore code and create form for the adding new subject

--- content.php
+++ content.php
@@ -1,49 +1,13 @@
 <?php require_once("includes/db_connection.php"); ?>
 <?php require_once("includes/functions.php"); ?>
-<?php
-if (isset($_GET['subj'])) {
-    $selected_subject = get_subject_by_id($_GET['subj']);
-    $selected_page = NULL;
-} elseif (isset($_GET['page'])) {
-    $selected_subject = NULL;
-    $selected_page = get_page_by_id($_GET['page']);
-} else {
-    $selected_subject = NULL;
-    $selected_page = NULL;
-}
-?>
+<?php find_selected_page(); ?>
 <?php include("includes/header.php"); ?>
 <table id="structure">
     <tr>
         <td id="navigation">
-            <ul class="subjects">
-                <?php
-                $subject_set = get_all_subjects();
-                if ($subject_set->num_rows > 0) {
-                    while ($subject = $subject_set->fetch_assoc()) {
-                        echo "<li";
-                        if ($selected_subject && $subject["id"] == $selected_subject["id"]) {
-                            echo " class=\"selected\"";
-                        }
-                        echo "><a href=\"content.php?subj=" . urlencode($subject["id"]) .
-                            "\">{$subject["menu_name"]}</a></li>";
-
-                        $page_set = get_pages_for_subjects($subject["id"]);
-                        if ($page_set->num_rows > 0) {
-                            echo "<ul class='pages'>";
-                            while ($page = $page_set->fetch_assoc()) {
-                                echo "<li";
-                                if ($selected_page && $page["id"] == $selected_page["id"]) {
-                                    echo " class=\"selected\"";
-                                }
-                                echo "><a href=\"content.php?page=" . urlencode($page["id"]) . "\">{$page["menu_name"]}</a></li>";
-                            }
-                            echo "</ul>";
-                        }
-                    }
-                }
-                ?>
-            </ul>
+            <?php echo navigation($selected_subject, $selected_page); ?>
+            <br>
+            <a href="new_subject.php">+Add a new subject</a>
         </td>
         <td id="page">
             <?php if (!is_null($selected_subject)) { //subject selected 
--- includes/functions.php
+++ includes/functions.php
@@ -70,3 +70,50 @@ function get_page_by_id($page_id)
         return NULL;
     }
 }
+
+function find_selected_page()
+{
+    global $selected_subject;
+    global $selected_page;
+    if (isset($_GET['subj'])) {
+        $selected_subject = get_subject_by_id($_GET['subj']);
+        $selected_page = NULL;
+    } elseif (isset($_GET['page'])) {
+        $selected_subject = NULL;
+        $selected_page = get_page_by_id($_GET['page']);
+    } else {
+        $selected_subject = NULL;
+        $selected_page = NULL;
+    }
+}
+
+function navigation($selected_subject, $selected_page)
+{
+    $output = "<ul class=\"subjects\">";
+    $subject_set = get_all_subjects();
+    if ($subject_set->num_rows > 0) {
+        while ($subject = $subject_set->fetch_assoc()) {
+            $output .= "<li";
+            if ($selected_subject && $subject["id"] == $selected_subject["id"]) {
+                $output .= " class=\"selected\"";
+            }
+            $output .= "><a href=\"content.php?subj=" . urlencode($subject["id"]) .
+                "\">{$subject["menu_name"]}</a></li>";
+
+            $page_set = get_pages_for_subjects($subject["id"]);
+            if ($page_set->num_rows > 0) {
+                $output .= "<ul class='pages'>";
+                while ($page = $page_set->fetch_assoc()) {
+                    $output .= "<li";
+                    if ($selected_page && $page["id"] == $selected_page["id"]) {
+                        $output .= " class=\"selected\"";
+                    }
+                    $output .= "><a href=\"content.php?page=" . urlencode($page["id"]) . "\">{$page["menu_name"]}</a></li>";
+                }
+                $output .= "</ul>";
+            }
+        }
+    }
+    $output .= "</ul>";
+    return $output;
+}
